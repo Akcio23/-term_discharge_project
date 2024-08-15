7fefcc3b6d9d1c98b3bde7d5119fc9b9afc5c257
AKcio a <dev.akcio@gmail.com>
2024-08-15 19:29:57 -0300

    start

---
+++ index.js
@@ -0,0 +1,36 @@
+
+import * as fs from "fs";
+const docx = require("docx");
+
+import { Document, Packer, Paragraph, TextRun } from "docx";
+
+
+const doc = new Document({
+    sections: [
+        {
+            properties: {},
+            children: [
+                new Paragraph({
+                    children: [
+                        new TextRun("Hello World"),
+                        new TextRun({
+                            text: "Foo Bar",
+                            bold: true,
+                        }),
+                        new TextRun({
+                            text: "\tGithub is the best",
+                            bold: true,
+                        }),
+                    ],
+                }),
+            ],
+        },
+    ],
+});
+
+// Used to export the file into a .docx file
+Packer.toBuffer(doc).then((buffer) => {
+    fs.writeFileSync("My Document.docx", buffer);
+});
+
+// Done! A file called 'My Document.docx' will be in your file system.
